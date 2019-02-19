6e24847a5ae29cba5bffd1fa2b7811ecc90882f1
Eric <el172@duke.edu>
2019-02-19 02:16:19 -0500

    start screen draft finished

--- src/StartButton.java
+++ src/StartButton.java
@@ -5,7 +5,7 @@ public class StartButton extends Button {
 
     public static final String ENTER_TEXT = "Start SLogo Program";
     public static final Color TEXT_COLOR = Color.WHITE;
-    public static final double BUTTON_WIDTH = 150;
+    public static final double BUTTON_WIDTH = 180;
 
     public StartButton() {
         super(ENTER_TEXT);
