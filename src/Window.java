b7d7f296bc82e74d4b95a2453b83372ea9a8a99f
Eric <el172@duke.edu>
2019-02-19 17:15:01 -0500

    small changes to starting window

--- src/Window.java
+++ src/Window.java
@@ -8,8 +8,8 @@ public class Window extends Scene {
     private double height;
     private Group root;
 
-    public Window(Group root, double width, double height) {
-        super(root, width, height);
+    public Window(Group root, double width, double height, Color background) {
+        super(root, width, height, background);
         this.root = root;
         this.width = width;
         this.height = height;
