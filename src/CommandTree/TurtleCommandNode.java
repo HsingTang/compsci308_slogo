83a6728f0356729793af330b7f3c1d1a13574e51
Mary Gooneratne <mmg53@duke.edu>
2019-02-24 17:47:56 -0500

    Fleshed out model some more

--- src/CommandTree/TurtleCommandNode.java
+++ src/CommandTree/TurtleCommandNode.java
@@ -1,15 +1,24 @@
 package CommandTree;
 
+import java.util.ArrayList;
+
 public class TurtleCommandNode extends CommandNode{
    private int value;
+   private ArrayList<Double> parameters;
 
    public TurtleCommandNode(int val){
       super();
       this.setType("turtle");
       this.value = val;
+      this.parameters = new ArrayList<>();
    }
 
    public int getValue(){
       return this.value;
    }
+
+   public ArrayList<Double> getParameters(){
+      return this.parameters;
+   }
+
 }
--- src/Models/TurtleModel.java
+++ src/Models/TurtleModel.java
@@ -1,4 +1,103 @@
 package Models;
 
+import CommandTree.CommandNode;
+import CommandTree.TurtleCommandNode;
+import Turtles.TurtleView;
+import javafx.scene.paint.Color;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TurtleModel {
+   private static int VISIBLE = 1;
+   private static int INVISIBLE = 0;
+   private static Double INITIAL_POSITION = 0.0;
+
+   private HashMap<String, Method> methodMap;
+
+   private Double myX;
+   private Double myY;
+   private Double myXDir;
+   private Double myYDir;
+   private Color myPenColor;
+   private boolean penDown;
+   private TurtleView myView;
+
+   public TurtleModel(TurtleView myView){
+      this.myView = myView;
+      this.myX = INITIAL_POSITION;
+      this.myY = INITIAL_POSITION;
+      this.myXDir = INITIAL_POSITION;
+      this.myYDir = INITIAL_POSITION;
+      this.methodMap = new HashMap<>();
+   }
+
+   public void execute(TurtleCommandNode command){
+      Method method = methodMap.get(command.getType());
+      ArrayList<Double> parameters = command.getParameters();
+      try {
+         method.invoke(this, parameters);
+      }
+      catch(Exception e){
+         System.out.println("not valid!");
+      }
+   }
+
+   private void setMethodMap(){
+      Method [] methods = this.getClass().getDeclaredMethods();
+      for(Method m: methods){
+         m.setAccessible(true);
+         this.methodMap.put(m.getName(), m);
+      }
+   }
+
+   private Double forward(ArrayList<Double> params){
+      return params.get(0);
+   }
+
+   private Double backward(ArrayList<Double> params){
+      return params.get(0);
+   }
+
+   private Double left(ArrayList<Double> params){
+      return params.get(0);
+   }
+
+   private Double right(ArrayList<Double> params){
+      return params.get(0);
+   }
+
+   private Double setHeading(ArrayList<Double> params){
+      return params.get(0);
+   }
+
+   private Double towards(ArrayList<Double> params){
+      return params.get(0);
+   }
+
+   private int penUp(){
+      return VISIBLE;
+   }
+
+   private int penDown(){
+      return VISIBLE;
+   }
+
+   private int showTurtle(){
+      return VISIBLE;
+   }
+
+   private int hideTurtle(){
+      return INVISIBLE;
+   }
+
+   private Double home(){
+      Double distance = 0.0;
+      return distance;
+   }
+
+   private Double clearScreen(){
+      return this.home();
+   }
 }
