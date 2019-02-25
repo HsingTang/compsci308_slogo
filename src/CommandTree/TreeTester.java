f77c2bf4b9362997883efccdbf3742a6c8251b40
Duc Tran <dt135@duke.edu>
2019-02-25 13:17:04 -0500

    parsing command done

--- src/CommandTree/TreeTester.java
+++ src/CommandTree/TreeTester.java
@@ -6,7 +6,11 @@ public class TreeTester {
       String s = "forward 5 repeat 3 [ forward 2 forward 3 repeat 2 [ forward 5 ] ] forward 2";
       var parse = new StringParser();
       parse.addPatterns("languages/English");
-      parse.parseCommand("a b c d e #c dd");
+      parse.addPatterns("languages/Syntax");
+      var temp = parse.parseCommand("fd 90 devant 40 :dog ase #c dd");
+      for(var q : temp){
+         System.out.println(q);
+      }
       //CommandRoot root = new CommandRoot(parse.getCommandStrings());
       //CommandTreeController control = new CommandTreeController(root);
       //control.execute(root.getParent());
