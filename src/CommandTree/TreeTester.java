74eb95a45d365863c016e74d06bec66536472be7
Duc Tran <dt135@duke.edu>
2019-02-26 17:47:58 -0500

    pushing merge

--- src/CommandTree/TreeTester.java
+++ src/CommandTree/TreeTester.java
@@ -12,16 +12,12 @@ public class TreeTester {
    public static void main(String[] args){
       String s = "forward 5 repeat 3 [ forward 2 forward 3 repeat 2 [ forward 5 ] ] forward 2";
       var parse = new StringParser();
-      parse.addPatterns("languages/French");
+      parse.addPatterns("languages/English");
       parse.addPatterns("languages/Syntax");
-      var temp = parse.parseCommand("dev 50 der 40 :var [ gc 45 ] #c");
-      for(var e : temp){
-         System.out.println(e);
-      }
       String[] commands = {"forward", "5", "repeat", "3", "[", "forward", "2", "forward", "3", "repeat", "2", "[", "forward", "5", "]", "]", "forward", "2"};
-      CommandRoot root = new CommandRoot(commands);
+      //CommandRoot root = new CommandRoot(commands);
       MainModel model = new MainModel();
-      model.setCurrentRoot(root);
+      //model.setCurrentRoot(root);
       model.execute();
    }
 
