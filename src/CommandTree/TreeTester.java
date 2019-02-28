fe5eee8ec29fd64008f470f8ad1d39c20172a17c
Duc Tran <dt135@duke.edu>
2019-02-27 21:29:29 -0500

    preparing to merge

--- src/CommandTree/TreeTester.java
+++ src/CommandTree/TreeTester.java
@@ -1,10 +1,5 @@
 package CommandTree;
 
-import Handlers.CommandHandler;
-import Handlers.HandlerInterfaces.CommandHandlerInterface;
-import Model.TurtleModel;
-import Model.ModelInterfaces.ModelInterface;
-
 public class TreeTester {
 
    public static void main(String[] args){
