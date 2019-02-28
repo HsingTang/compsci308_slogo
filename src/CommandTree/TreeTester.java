ac5b6e2dbfc9b4c6d9e82d13ae5d9da1b988d8c5
Mary Gooneratne <mmg53@duke.edu>
2019-02-27 19:44:05 -0500

    stuff

--- src/CommandTree/TreeTester.java
+++ src/CommandTree/TreeTester.java
@@ -8,12 +8,14 @@ import Model.ModelInterfaces.ModelInterface;
 public class TreeTester {
 
    public static void main(String[] args){
-      String[] commands = {"forward", "forward", "5"};
-
+      String commands = "forward 5 repeat 3 [ forward forward 10 repeat 3 [ left 2 ] ] backward 1";
+      StringParser parser = new StringParser();
+      parser.addPatterns("languages/English");
+      parser.addPatterns("languages/Syntax");
       //CommandRoot root = new CommandRoot(commands, control);
       ModelInterface model = new TurtleModel();
       CommandHandlerInterface handler = new CommandHandler(model);
-      CommandRoot root = new CommandRoot(commands, handler);
+      CommandRoot root = new CommandRoot(parser.parseCommand(commands), handler);
       root.execute();
    }
 
