82e3fbb717f24f405f4948fd5c55e791d0eba470
Eric <el172@duke.edu>
2019-03-06 20:02:26 -0500

    more smells

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class Model {
 
-   private final HashMap<String, Method> methodMap;
+   private final Map<String, Method> methodMap;
 
    public Model(){
       this.methodMap = new HashMap<>();
