c0027a75faf97fc3264225fff4117e3568db748f
Hsingchih Tang <ht114@duke.edu>
2019-03-08 21:40:49 -0500

    fixed canvas set color bug

--- src/View/GUIFeatures/Panes/CanvasPane.java
+++ src/View/GUIFeatures/Panes/CanvasPane.java
@@ -2,20 +2,27 @@ package View.GUIFeatures.Panes;
 
 import View.Turtles.TurtleView;
 import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 
 public class CanvasPane extends StackPane {
+    public static final Color DEFAULT_BG_COLOR = Color.BLACK;
+
     private SlogoCanvas myCanvas;
+    private Canvas myBackGroundCanvas;
     private double myHeight;
     private double myWidth;
+    private Color myBackgroundColor;
 
     public CanvasPane(double width, double height){
         super();
         myHeight = height;
         myWidth = width;
-        this.setMaxSize(myWidth, myHeight);
-        initCanvas();
+        myBackgroundColor = DEFAULT_BG_COLOR;
+        setMaxSize(myWidth, myHeight);
+        initBackGround();
+        initSlogoCanvas();
     }
 
     public SlogoCanvas getCanvas(){
@@ -30,21 +37,33 @@ public class CanvasPane extends StackPane {
         return myHeight;
     }
 
+    public Color getColor() {
+        return this.myBackgroundColor;
+    }
+
     public void initTurtleView(TurtleView turtle){
         this.getChildren().add(turtle.getImgView());
-        turtle.setPen(new SlogoPen(myCanvas));
+        turtle.setPen(new SlogoPen(myCanvas,this));
     }
 
     public void setCanvasColor(Color color){
-        myCanvas.setBackgroundColor(color);
+        myBackgroundColor = color;
+        myBackGroundCanvas.getGraphicsContext2D().setFill(color);
+        myBackGroundCanvas.getGraphicsContext2D().fillRect(0, 0, myWidth, myHeight);
+        myBackGroundCanvas.toBack();
     }
 
-    private void initCanvas() {
+    private void initSlogoCanvas() {
         myCanvas = new SlogoCanvas(myWidth,myHeight);
         StackPane.setAlignment(myCanvas, Pos.CENTER);
         this.getChildren().add(myCanvas);
     }
 
-
+    private void initBackGround(){
+        myBackGroundCanvas = new Canvas(myWidth,myHeight);
+        myBackGroundCanvas.getGraphicsContext2D().setFill(myBackgroundColor);
+        myBackGroundCanvas.getGraphicsContext2D().fillRect(0, 0, myWidth, myHeight);
+        this.getChildren().add(myBackGroundCanvas);
+    }
 
 }
