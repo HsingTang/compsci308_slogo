9b20cc60eb724dcf7648d94e68b2043fc76cfae8
Eric <el172@duke.edu>
2019-03-21 19:01:22 -0400

    added a masterpiece class

--- src/CommandNodes/TreeParentNode.java
+++ src/CommandNodes/TreeParentNode.java
@@ -5,7 +5,12 @@ import Handlers.HandlerInterfaces.CommandHandlerInterface;
 import View.ObserverInterfaces.TurtleObserver;
 
 /**
- * CommandNode that serves as the top-most node of all CommandRoots
+ * CommandNode that serves as the top-most node of all CommandRoots. I've also added this to my masterpiece
+ * because it shows that when all the commands have finished executing, the update method is called onto the
+ * turtle observer. The update method will get all the necessary new model states and change them in order!
+ *
+ * @author Mary
+ * @author Eric Lin
  */
 public class TreeParentNode extends CommandNode{
 
